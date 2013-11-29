174f34cc6b3a83a3b838564071f0e23df935d7ee
Евгений Сафронов <npu3pa4huk@gmail.com>
2013-11-29 12:00:43 +0400

    Tests deleted

--- test/gen/ru/npu3pak/rpg/tests/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package ru.npu3pak.rpg.tests;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
